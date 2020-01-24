88f3dbd47c17216efc61b48e2b09a58baa008743
zaenur <zaenur.rochman98@gmail.com>
2020-01-24 21:13:44 +0700

    add base controller and model
     - add MY_Controller
     - add MY_Model

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,28 @@
+<?php
+class MY_Controller extends CI_Controller{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    function userIsLoggedIn(){
+        if (isset($this->session->userdata['user_data']['status']) && $this->session->userdata['user_data']['status'] == "true") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function adminIsLoggedIn(){
+        if (isset($this->session->userdata['admin_data']['status']) && $this->session->userdata['admin_data']['status'] == "true" ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
