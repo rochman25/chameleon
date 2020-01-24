4027195c8065f32b12259da5ee8614860110921a
zaenur <zaenur.rochman98@gmail.com>
2020-01-24 21:10:32 +0700

    init project
     - add database

---
+++ db_chameleon.sql
@@ -0,0 +1,280 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 24 Jan 2020 pada 15.10
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_chameleon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` varchar(36) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('1','0') NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `alamat_pengguna`
+--
+
+CREATE TABLE `alamat_pengguna` (
+  `id_alamat` varchar(36) NOT NULL,
+  `id_pengguna` varchar(36) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `alamat_1` text NOT NULL,
+  `alamat_2` text NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `kota` varchar(100) NOT NULL,
+  `kode_pos` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart_item`
+--
+
+CREATE TABLE `cart_item` (
+  `id_cart` varchar(36) NOT NULL,
+  `id_pengguna` varchar(36) NOT NULL,
+  `id_produk` varchar(36) NOT NULL,
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` varchar(36) NOT NULL,
+  `kode_transaksi` varchar(30) NOT NULL,
+  `id_produk` varchar(36) NOT NULL,
+  `jumlah_produk` int(3) NOT NULL,
+  `total` float NOT NULL,
+  `discount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` varchar(36) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `deskripsi_kategori` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` varchar(36) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` varchar(36) NOT NULL,
+  `kode_produk` varchar(30) NOT NULL,
+  `nama_produk` varchar(150) NOT NULL,
+  `deskripsi_produk` text NOT NULL,
+  `stok_produk` int(3) NOT NULL,
+  `harga_produk` float NOT NULL,
+  `id_kategori` varchar(36) NOT NULL,
+  `warna_produk` varchar(30) NOT NULL,
+  `size_produk` varchar(10) NOT NULL,
+  `thumbnail_produk` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `profile_toko`
+--
+
+CREATE TABLE `profile_toko` (
+  `id_toko` varchar(36) NOT NULL,
+  `nama_toko` varchar(30) NOT NULL,
+  `deskripsi_toko` text NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `email` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(36) NOT NULL,
+  `kode_transaksi` varchar(30) NOT NULL,
+  `id_pengguna` varchar(36) NOT NULL,
+  `id_alamat` varchar(36) NOT NULL,
+  `waktu_transaksi` datetime NOT NULL,
+  `waktu_expired` datetime NOT NULL,
+  `total_harga` float NOT NULL,
+  `total_ongkir` float NOT NULL,
+  `status_transaksi` varchar(20) NOT NULL,
+  `bukti_transfer` varchar(40) NOT NULL,
+  `no_resi` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `alamat_pengguna`
+--
+ALTER TABLE `alamat_pengguna`
+  ADD PRIMARY KEY (`id_alamat`),
+  ADD KEY `fk_alamat_pengguna` (`id_pengguna`);
+
+--
+-- Indexes for table `cart_item`
+--
+ALTER TABLE `cart_item`
+  ADD PRIMARY KEY (`id_cart`),
+  ADD KEY `fk_cart_pengguna` (`id_pengguna`),
+  ADD KEY `fk_cart_produk` (`id_produk`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `fk_detail_transaksi` (`id_transaksi`),
+  ADD KEY `fk_kode_transaksi` (`kode_transaksi`),
+  ADD KEY `fk_detail_produk_transaksi` (`id_produk`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD UNIQUE KEY `kode_produk` (`kode_produk`),
+  ADD KEY `fk_produk_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `profile_toko`
+--
+ALTER TABLE `profile_toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD UNIQUE KEY `kode_transaksi` (`kode_transaksi`),
+  ADD KEY `fk_transaksi_pengguna` (`id_pengguna`),
+  ADD KEY `fk_alamat` (`id_alamat`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `alamat_pengguna`
+--
+ALTER TABLE `alamat_pengguna`
+  ADD CONSTRAINT `fk_alamat_pengguna` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`);
+
+--
+-- Ketidakleluasaan untuk tabel `cart_item`
+--
+ALTER TABLE `cart_item`
+  ADD CONSTRAINT `fk_cart_pengguna` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`),
+  ADD CONSTRAINT `fk_cart_produk` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+
+--
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `fk_detail_produk_transaksi` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`),
+  ADD CONSTRAINT `fk_detail_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `fk_kode_transaksi` FOREIGN KEY (`kode_transaksi`) REFERENCES `transaksi` (`kode_transaksi`);
+
+--
+-- Ketidakleluasaan untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `fk_produk_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_alamat` FOREIGN KEY (`id_alamat`) REFERENCES `alamat_pengguna` (`id_alamat`),
+  ADD CONSTRAINT `fk_transaksi_pengguna` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
