babe91b349582e6def9b83c755d49a860bc5cd0b
Trian Damai <triannurizkillah@gmail.com>
2020-02-29 06:41:42 +0700

    view confirm

--- application/views/public/konfirmasi-pembayaran.php
+++ application/views/public/konfirmasi-pembayaran.php
@@ -13,6 +13,16 @@ $this->load->view('public/cart');
 
 <section id="content">
 
+			<div class="container">
+				<section>
+                <form action="" method="post">
+                    <div class="title">Catatan untuk Chameleon cloth (optional)</div>
+                    <textarea name="catatan" class="notes"></textarea>
+                    <input type="submit" name="kirim" value="BELI">
+                </form>
+				</section>
+			</div>
+
 </section>
 
 <?php
