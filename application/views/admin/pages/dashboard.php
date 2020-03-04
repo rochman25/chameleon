9d929db21e9a1ab841a1ef1b7fa5b7398b3f89dc
rochman25 <zaenur.rochman98@gmail.com>
2020-03-04 15:12:47 +0700

    fix bug and error

--- application/views/admin/pages/dashboard.php
+++ application/views/admin/pages/dashboard.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Chameleon Dashboard Page</title>
@@ -28,8 +28,6 @@
                                             <div class="card-stats-item-label">Pending</div>
                                         </div>
                                         <div class="card-stats-item">
-<<<<<<< HEAD
-=======
                                             <div class="card-stats-item-count"><?= $statistik['validasi'] ?></div>
                                             <div class="card-stats-item-label">Validasi</div>
                                         </div>
@@ -38,7 +36,6 @@
                                             <div class="card-stats-item-label">Proses</div>
                                         </div>
                                         <div class="card-stats-item">
->>>>>>> f641e3f56620d12ee73035d81510aac9787620cc
                                             <div class="card-stats-item-count"><?= $statistik['kirim'] ?></div>
                                             <div class="card-stats-item-label">Kirim</div>
                                         </div>
