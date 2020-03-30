2abdd8dfe38606a3e335f764a205350e41f75c3c
rochman25 <zaenur.rochman98@gmail.com>
2020-03-30 22:15:38 +0700

    update footer

--- application/views/public/footer.php
+++ application/views/public/footer.php
@@ -3,69 +3,77 @@
         <div class="footer-list">
             <h1>koleksi</h1>
             <ul>
-                <li><a href="<?= base_url()?>produk/jas">Jas</a></li>
-                <li><a href="<?= base_url()?>produk/kemeja">Kemeja</a></li>
-                <li><a href="<?= base_url()?>produk/celana">Celana</a></li>
+                <li><a href="<?= base_url() ?>produk/jas">Jas</a></li>
+                <li><a href="<?= base_url() ?>produk/kemeja">Kemeja</a></li>
+                <li><a href="<?= base_url() ?>produk/celana">Celana</a></li>
             </ul>
         </div>
 
         <div class="footer-list">
-           <h1>Tentang Kami</h1>
-           <ul> 
+            <h1>Tentang Kami</h1>
+            <ul>
                 <li><a href="#">Kontak &amp; Alamat</a></li>
-                <li>Alamat: 
+                <li><a href="">Alamat:</a>
                     <ul>
                         <li><a href="">CHAMLEON CLOTH, Jl. Patimuan - Kedungreja, Cinyawang, Rt.02/03, Kec.Patimuan - Cilacap 53264</a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="">Nomor : 083116200500</a>
-                </li>
-                <li>
-                    <a href="">Email : cs@chameleoncloth.co.id</a>
+                    <a href="">Kontak:</a>
+                    <ul>
+                        <li>
+                            <a href="">Nomor : 083116200500</a>
+                        </li>
+                        <li>
+                            <a href="">Email : cs@chameleoncloth.co.id</a>
+                        </li>
+                        <li>
+                            <a href="">LINE ID : chameleoncloth</a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
         <div class="footer-list">
             <h1>Layanan</h1>
             <ul>
-                <li><a href="<?= base_url()?>panduan/pemesanan"">Panduan Pemesanan</a></li>
-                <li><a href="<?= base_url()?>panduan/ukuran"">Panduan Ukuran</a></li>
-                <li><a href="<?= base_url()?>panduan/retur"">Panduan Retur</a></li>
+                <li><a href="<?= base_url() ?>panduan/pemesanan"">Panduan Pemesanan</a></li>
+                <li><a href=" <?= base_url() ?>panduan/ukuran"">Panduan Ukuran</a> </li> <li><a href="<?= base_url() ?>panduan/retur"">Panduan Retur</a></li>
             </ul>
         </div>
-        <div class="pull-right-footer">
-            <div class="social">
-                <div class="socialmedia-item">
-                    <a href="https://facebook.com/chameleonclothindonesia" target="blank"><i class="fa fa-facebook fa-2x"></i></a>
-                </div>
-                <div class="socialmedia-item">
-                    <a href="https://mobile.twitter.com/chameleoncloth_" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
-                </div>
-                <div class="socialmedia-item">
-                    <a href="https://instagram.com/chameleoncloth.co.id" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
-                </div>
-                <div class="socialmedia-item">
-                    <a href="https://m.youtube.com/channel/UCDdDpkjX-TVzufeMUq3D0Hw" target="_blank"><i class="fa fa-youtube fa-2x"></i></a>
-                </div>
-                <div class="socialmedia-item">
-                    <a href="https://api.whatsapp.com/send?phone=6283116200500" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
-                </div>
-            </div>
+        <div class=" pull-right-footer">
+                            <div class="social">
+                                <div class="socialmedia-item">
+                                    <a href="https://facebook.com/chameleonclothindonesia" target="blank"><i class="fa fa-facebook fa-2x"></i></a>
+                                </div>
+                                <div class="socialmedia-item">
+                                    <a href="https://mobile.twitter.com/chameleoncloth_" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
+                                </div>
+                                <div class="socialmedia-item">
+                                    <a href="https://instagram.com/chameleoncloth.co.id" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+                                </div>
+                                <div class="socialmedia-item">
+                                    <a href="https://m.youtube.com/channel/UCDdDpkjX-TVzufeMUq3D0Hw" target="_blank"><i class="fa fa-youtube fa-2x"></i></a>
+                                </div>
+                                <div class="socialmedia-item">
+                                    <a href="https://api.whatsapp.com/send?phone=6283116200500" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
         </div>
     </div>
 
     <div class="container copyright">
-        Copyright 2020 &copy; <a href="chameleoncloth.co.id">chameleoncloth.co.id</a>     
+        Copyright 2020 &copy; <a href="chameleoncloth.co.id">chameleoncloth.co.id</a>
     </div>
 </footer>
 
 
 
 <script type="text/javascript">
-	$(window).on('load',function(){
-		$(".svg-container").fadeOut("slow");
+    $(window).on('load', function() {
+        $(".svg-container").fadeOut("slow");
     });
 </script>
-    </body>
+</body>
+
 </html>
